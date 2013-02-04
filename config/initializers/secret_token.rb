f23a7ba33b0c1c3f3d539377a9727c9159757d5d
Ubuntu <ubuntu@ip-10-242-50-173.ec2.internal>
2013-02-04 21:45:18 +0000

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwissMiss::Application.config.secret_token = '716df4a9eb918225fb6b23822cf00ee579fde3364628a7b9660bafff944e37b43db9f4c5c69c5a021fa4567d3e4a132a2217716238a5c5a054b10004f24f4598'
